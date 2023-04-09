ca134eedbe525de39201b1d178e26bff479bca55
AustinWinslow <austin@austinwinslow.com>
2023-04-09 14:47:29 -0700

    added example for displaying user data on manage.html, fixes to get app to working state again

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, redirect, url_for, flash
+from flask import Flask, render_template, redirect, url_for, flash, request, jsonify
+from forms import login_form, registration_form, account_form, generator_form
 from forms.registration_form import RegistrationForm as registerForm
 from forms.login_form import LoginForm as loginForm
 from forms.account_form import AccountForm as accountForm
@@ -26,8 +27,10 @@ currentUsr = None
 
 @app.route('/register/', methods=['GET', 'POST'])
 def register():
-    # allow access to global variable current_user
+    # allow access to global variables
     global current_user
+    global accounts
+    global dev_env
     form = registerForm()
     # validation occurs on form submission
     if form.validate_on_submit():
@@ -64,8 +67,10 @@ def register():
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
-    # allow access to global current_user object
+    # allow access to global variables
     global current_user
+    global accounts
+    global dev_env
     # change to jwt authentication
     form = loginForm()
     test_err_msg = 'test error message'
@@ -107,23 +112,32 @@ def login():
 
 @app.route('/manage/<usrname>', methods=['GET', 'POST'])
 def manage(usrname):
-    # get accounts then create list of all accounts
+    # allow access to global variables
+    global current_user
+    global accounts
+    global dev_env
+    # set dev, remove when done
+    dev_env = True
     form = accountForm()
     if not dev_env:
         # get list of currentUsr's accounts
-        accounts = acount_db.fetch_user_accounts(current_user.usr_id, current_user.username)
+        accounts = acount_db.fetch_user_accounts(current_user.usr_id, username)
     else:
          # create dev_user, add account; remove when done with development
          current_user = User("dev_user", "hzdkv@example.com")
          current_user.user_id = "0"
          account = Account('00000aaa', 0, 'gmail', 'zdev1@example.com', 'plainTxt')
          accounts.append(account)
-    return render_template('manage.html', current_user=current_user, username = current_user.username, form=form)
+    return render_template('manage.html', current_user=current_user, username = current_user.username, form=form, test_data=test_data)
 
 
 @app.route('/generator/<username>', methods=['GET', 'POST'])
 def generator(username):
     global current_user
+    global accounts
+    global dev_env
+    if dev_env is True:
+        current_user = User("dev_user", "hzdkv@example.com")
     form = generatorForm()
     if form.validate_on_submit():
         # get password criteria from user
@@ -142,7 +156,29 @@ def generator(username):
         form.generated_password.data = generated_password
         # check is useless if all cases return same redirect
         if generated_password is not None:
-            return render_template('generator.html', current_user=current_user, form=form)
+            return render_template('generator.html', current_user=current_user, form=form, username = current_user.username)
         else:
-            return render_template('generator.html', current_user=current_user, form=form)
-    return render_template('generator.html', current_user=current_user, username = current_user.username, form=form)
+            return render_template('generator.html', current_user=current_user, form=form, username = current_user.username)
+    return render_template('generator.html', current_user=current_user, username=current_user.username, form=form)
+
+
+# example for updating dictionary value (see manage.html for context)
+@app.route('/update', methods=['POST'])
+def update():
+    global current_user
+    global accounts
+    global dev_env
+    updated_data = request.get_json()
+    key = updated_data.get('key')
+    value = updated_data.get('value')
+
+    if key in test_data:
+        test_data[key] = value
+        return jsonify({'status': 'success', 'message': f'Updated {key} to {value}'}), 200
+    else:
+        return jsonify({'status': 'error','message': f'Key {key} not found'}), 400
+    
+
+
+# use this so we can just run "python app.py" in terminal
+app.run(debug=True)
--- forms/generator_form.py
+++ forms/generator_form.py
@@ -5,9 +5,11 @@ from wtforms.validators import DataRequired
 
 class GeneratorForm(FlaskForm):
     # need to add validation that the number of characters doesnt go over len
-    pwd_length = IntegerField('Length', validators=[DataRequired()])
+    password_length = IntegerField('Length', validators=[DataRequired()])
     letter_count = IntegerField('Letter Count', validators=[DataRequired()])
     number_count = IntegerField('Number Count')
     symbol_count = IntegerField('Symbol Count')
-    generated_pwd = StringField()
+    generated_password = StringField()
     generate = SubmitField('Generate')
+    if generated_password is not None:
+        password_generated = True
