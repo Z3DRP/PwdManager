0c7defa83adeed24dae315b6c75a07fa550f17a6
AustinWinslow <austin@austinwinslow.com>
2023-04-10 14:53:58 -0700

    reverted all of my variable name changes to match with main branch, fixed username in generator url issue

--- app.py
+++ app.py
@@ -22,44 +22,48 @@ app.config['SECRET_KEY'] = get_secret()
 usrID = ''
 accounts = []
 account = {""}
+#currentUsr should be a user object
 currentUsr = None
+# set dev, remove when done
+dev_env = True
+#create dev_user, add account; remove when done with development
+if dev_env:
+         currentUsr = User("dev_user", "hzdkv@example.com")
+         currentUsr.userID = "0"
+         account = Account('00000aaa', 0, 'gmail', 'zdev1@example.com', 'plainTxt')
+         accounts.append(account)
 
 
 @app.route('/register/', methods=['GET', 'POST'])
 def register():
     # allow access to global variables
-    global current_user
+    global currentUsr
     global accounts
     global dev_env
+    # create user save usr to db then redirect to home
     form = registerForm()
     # validation occurs on form submission
     if form.validate_on_submit():
         # EqaulTo validator validates passwords match dont need to add the logic
         try:
             # get user info to register new user
-            username = form.username.data
+            usrname = form.username.data
             email = form.email.data
-            password = form.password.data
+            pwd = form.password.data
             # initialize new user object
-            current_user = User(username, email)
-            # set user info and encrypt password
-            current_user.set_salt()
-            current_user.set_password(password)
-            current_user.set_key()
-            current_user.set_user_id()
+            currentUsr = User(usrname, email)
+            # set user info
+            currentUsr.set_password(pwd)
             # save user to database
-            successful_insert = user_db.insert_user(current_user)
-            # if successful, log user in, send success message, and set secret key to user's key
+            successful_insert = user_db.insert_user(currentUsr)
             if successful_insert:
-                flash("Logged in successfully!")
-                app.config['SECRET_KEY'] = current_user.get_key()
-                # redirect to home page pass through entire current_user object
-                return redirect(url_for('home', current_user=current_user, username = current_user.username))
+                # redirect to home page pass through entire currentUsr object
+                return redirect(url_for('home', currentUsr=currentUsr, usrname = currentUsr.usrname))
             else:
-                flash("Failed to register user!", category="error")
+                # need to show error if insert failed
                 return render_template('register.html', form=form)
         except Exception as err:
-            flash("Failed to register user!", category="error")
+            # figure out way to display error and success to usr
             print(err)
     else:
         return render_template('register.html', form=form)
@@ -68,40 +72,33 @@ def register():
 @app.route('/', methods=['GET', 'POST'])
 def login():
     # allow access to global variables
-    global current_user
+    global currentUsr
     global accounts
     global dev_env
     # change to jwt authentication
     form = loginForm()
     test_err_msg = 'test error message'
     if form.validate_on_submit():
-        username = form.username.data
-        plain_text_password = form.password.data
+        usrname = form.username.data
+        pwd = form.password.data
         # NOTE flash message goes from server to client, can show success/error with them. Not sure what these flashes do.
         # flash will output values somewhere im not sure where
         # %s might need changed
         flash('username: %s' % form.username.data)
-        flash('plaintext pw: %s' % form.pwd.data)
+        flash('plaintext pw: %s' % form.password.data)
         isMatch = User.verify_password(usrname, pwd)
-
+        # if password is correct, log user in
         if isMatch:
-            global currentUsr
-            currentUsr = usrname
             auth_failed = False
             accountform = accountForm()
             # fetch user info from database
-            user_info = user_db.fetch_user(username)
+            user_info = user_db.fetch_user(usrname)
             # initialize user object using user info from database
-            current_user = User(username, user_info['email'])
-            current_user.salt = user_info['salt']
+            current_user = User(usrname, user_info['email'])
             current_user.user_id = user_info['user_id']
-            current_user.set_key(plain_text_password)
-            # set secret key to user's key
-            app.config['SECRET_KEY'] = current_user.get_key()
-            # send success message to user
-            flash("Logged in successfully!")
-            # redirect to management page sending through entire current_user object
-            return redirect(url_for('manage', current_user=current_user, username = current_user.username, form=accountform))
+            # TODO: login success message display
+            # redirect to management page sending through entire currentUsr object
+            return redirect(url_for('manage', currentUsr=currentUsr, usrname = currentUsr.usrname, form=accountform))
         # on failed login, send error message, return to login page
         else:
             auth_failed = True
@@ -112,8 +109,7 @@ def login():
 
 @app.route('/manage/<usrname>', methods=['GET', 'POST'])
 def manage(usrname):
-    # allow access to global variables
-    global current_user
+    global currentUsr
     global accounts
     global dev_env
     # set dev, remove when done
@@ -121,63 +117,40 @@ def manage(usrname):
     form = accountForm()
     if not dev_env:
         # get list of currentUsr's accounts
-        accounts = acount_db.fetch_user_accounts(current_user.usr_id, username)
-    else:
-         # create dev_user, add account; remove when done with development
-         current_user = User("dev_user", "hzdkv@example.com")
-         current_user.user_id = "0"
-         account = Account('00000aaa', 0, 'gmail', 'zdev1@example.com', 'plainTxt')
-         accounts.append(account)
-    return render_template('manage.html', current_user=current_user, username = current_user.username, form=form, test_data=test_data)
+        accounts = acount_db.fetch_user_accounts(currentUsr.usr_id, usrname)
+    # returns to manage page, passes through currentUsr object, usrname and the user's accoutns
+    return render_template('manage.html', currentUsr=currentUsr, usrname = currentUsr.usrname, form=form, accounts=accounts)
 
 
-@app.route('/generator/<username>', methods=['GET', 'POST'])
-def generator(username):
-    global current_user
+@app.route('/generator/<usrname>', methods=['GET', 'POST'])
+def generator(usrname):
+    global currentUsr
     global accounts
     global dev_env
-    if dev_env is True:
-        current_user = User("dev_user", "hzdkv@example.com")
+    #if dev_env is True:
+    #    current_user = User("dev_user", "hzdkv@example.com")
     form = generatorForm()
     if form.validate_on_submit():
         # get password criteria from user
-        password_length = form.password_length.data
+        pwd_length = form.pwd_length.data
         letter_count = form.letter_count.data
-        number_count = form.letter_count.data
+        number_count = form.number_count.data
         symbol_count = form.symbol_count.data
         # generate password with criteria
-        generated_password = random_generator.generate_pwd(
-            password_length,
+        gen_pwd = random_generator.generate_pwd(
+            pwd_length,
             letter_count,
             number_count,
             symbol_count
         )
         # set password form to show generated password
-        form.generated_password.data = generated_password
+        form.generated_pwd.data = gen_pwd
         # check is useless if all cases return same redirect
-        if generated_password is not None:
-            return render_template('generator.html', current_user=current_user, form=form, username = current_user.username)
+        if gen_pwd is not None:
+            return render_template('generator.html', currentUsr=currentUsr, form=form, usrname = currentUsr.usrname)
         else:
-            return render_template('generator.html', current_user=current_user, form=form, username = current_user.username)
-    return render_template('generator.html', current_user=current_user, username=current_user.username, form=form)
-
-
-# example for updating dictionary value (see manage.html for context)
-@app.route('/update', methods=['POST'])
-def update():
-    global current_user
-    global accounts
-    global dev_env
-    updated_data = request.get_json()
-    key = updated_data.get('key')
-    value = updated_data.get('value')
-
-    if key in test_data:
-        test_data[key] = value
-        return jsonify({'status': 'success', 'message': f'Updated {key} to {value}'}), 200
-    else:
-        return jsonify({'status': 'error','message': f'Key {key} not found'}), 400
-    
+            return render_template('generator.html', currentUsr=currentUsr, form=form, usrname = currentUsr.usrname)
+    return render_template('generator.html', currentUsr=currentUsr, form=form, usrname = currentUsr.usrname)
 
 
 # use this so we can just run "python app.py" in terminal
--- models/User.py
+++ models/User.py
@@ -1,108 +1,127 @@
-import bcrypt
+import rsa
+from random import randint, seed
+from werkzeug.security import generate_password_hash, check_password_hash
+from data_access import user_db, auth_db
 from utils.random_generator import generate_id
-from utils.encryption import *
 
 
 class User:
-    def __init__(self, username=None, email=None):
-        self.username = username
+    def __init__(self, usrname=None, email=None):
+        self.usrname = usrname
         self.email = email
-        self.user_id = None
-        self.salt = None
-        self.encrypted_password = None
-        self.key = None
+        # init userID to none, when fetching user data, fill, it new user, we generate after user creation
+        self.userId = None
+        self.encrypted_pwd = None
 
 
-    # set user's salt, should only be called when creating a new user; salt is should be stored for reuse.
-    def set_salt(self):
+    #v1 pwd encryption methods
+    def set_password(self, plaintxt):
         try:
-            self.salt = bcrypt.gensalt()
+            was_success = None
+            public_key, private_key = self.get_keys()
+            self.encrypted_pwd = self.encrypt_pwd(plaintxt, public_key)
+            if self.encrypted_pwd is not None:
+                was_success = auth_db.store_keys(self.usrid, private_key, public_key)
+                print('success:: ' + was_success)
         except Exception as err:
             print(err)
 
-
-    def get_salt(self):
+    def get_keys(self):
         try:
-            return self.salt
+            public, private = rsa.newkeys(512)
+        except Exception as err:
+            print('Error::', err)
+        return [public, private]
+
+    @staticmethod
+    def fetch_public_key(self, usrname):
+        publickey = None
+        try:
+            publickey = auth_db.fetch_public_key(usrname)
         except Exception as err:
             print(err)
+        return publickey if publickey is not None else ''
 
-
-    # encrypts plain_text_password with user's salt, gets stored in the database.
-    def set_password(self, plain_text_password):
+    @staticmethod
+    def fetch_private_key(self, usrname):
+        privatekey = None
         try:
-            self.encrypted_password = encrypt_password(self.salt, plain_text_password)
+            privatekey = auth_db.fetch_private_key(usrname)
         except Exception as err:
             print(err)
+        return privatekey if privatekey is not None else ''
 
+    # v1 pwd encryption methods
+    def encrypt_pwd(self, plaintxt, publickey):
+        return rsa.encrypt(plaintxt.encode(), publickey)
+
+    def decrypt_pwd(self, encrypted_pwd, privatekey):
+        return rsa.decrypt(encrypted_pwd, self.private_key).decode()
+
+    # decryption to test if public key will work it should
+    def decrypt_pwd_public(self, encrypted_pwd, public_key):
+        return rsa.decrypt(encrypted_pwd, public_key).decode()
+
+    @staticmethod
+    def verify_password(self, usrname, entered_pwd):
+        usr = None
+        privatekey = None
+        try:
+            usr = user_db.fetch_user(usrname)
+            if usr is None:
+                raise ValueError('That user does not exist')
+            else:
+                privatekey = self.fetch_private_key(usrname)
+                if privatekey is None:
+                    raise ValueError('Unable to authenticate user')
+        except Exception as err:
+            print(err)
+        return self.decrypt_pwd(usr["username"], privatekey) == entered_pwd
+
+
+    #v2 pwd encryption
+    # def hash_password(self, plaintxt):
+    #     return generate_password_hash(plaintxt)
+    #
+    # def set_password(self, plaintxt, should_encrypt):
+    #     if should_encrypt:
+    #         self._pwd_hash = self.hash_password(plaintxt)
+    #     else:
+    #         self._pwd_hash = plaintxt
+    #
+    # def get_password_hash(self):
+    #     return self._pwd_hash
+    #
+    # def verify_password(self,  usrname, plaintxt):
+    #     try:
+    #         usr = user_db.fetch_user(usrname)
+    #         if usr is None:
+    #             raise Exception('That username does not exist')
+    #     except Exception as err:
+    #         print(err)
+    #     return check_password_hash(usr['_pwd_hash'], plaintxt)
+
+    #end v2 pwd encryption
+    def get_user_id(self):
+        return self.usrid
+
+    def set_username(self, usrname):
+        self.username = usrname
+
+    def get_username(self):
+        return self.username
+
+    def set_email(self, email):
+        self.email = email
+
+    def get_email(self):
+        return self.email
 
     def get_password(self):
-        try:
-            return self.encrypted_password
-        except Exception as err:
-            print(err)
+        return self.encrypted_pwd
+    # maybe add isValid method to check data is correct types
+
+    def plain_text_password(self):
+        self.encrypted_pwd
 
 
-    # generates a user's key; call on user login, we don't store the key in the database.
-    def set_key(self, plain_text_password):
-        try:
-            self.key = generate_key(self.encrypted_password, plain_text_password, self.salt)
-        except Exception as err:
-            print(err)
-
-
-    def get_key(self):
-        try:
-            return self.key
-        except Exception as err:
-            print(err)
-
-
-    # generates a user's user_id; call only once when creating a new user, is stored in database.
-    def set_user_id(self):
-        try:
-            self.user_id = generate_id()
-        except Exception as err:
-            print(err)
-
-
-    def get_user_id(self):
-        try:
-            return self.user_id
-        except Exception as err:
-            print(err)
-
-
-    def set_username(self, username):
-        try:
-            self.username = username
-        except Exception as err:
-            print(err)
-
-    
-    def get_username(self):
-        try:
-            return self.username
-        except Exception as err:
-            print(err)
-    
-    
-    def set_email(self, email):
-        try:
-            self.email = email
-        except Exception as err:
-            print(err)
-
-    
-    def get_email(self):
-        try:
-            return self.email
-        except Exception as err:
-            print(err)
-
-    
-    def get_user_id(self):
-        try:
-            return self.user_id
-        except Exception as err:
-            print(err)
